4f9d2544c983e140f2dd8570ced0a97d2b96875f
BigBrainAFK <tony.langhammer@yahoo.de>
2021-12-31 20:17:21 +0100

    Fix passport not working on newer firefox

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
 const server = { ex: "https://exhentai.org", forums: "https://forums.e-hentai.org" };
 
 async function modifyCookie(message, sender, sendResponse) {
-
-    let pass_hash = null;
-    let member_id = null;
-
-    for (let name of ["ipb_member_id", "ipb_pass_hash", "yay"]) {
+    for (const name of ["ipb_member_id", "ipb_pass_hash", "yay", "igneous"]) {
+        if (name === "igneous") {
+            const igneous = await browser.cookies.get({ url: server.ex, name: name });
+            if (igneous.value !== 'mystery') continue;
+        }
         await browser.cookies.remove({ url: server.ex, name: name });
     }
 
-    pass_hash = await browser.cookies.get({ url: server.forums, name: "ipb_pass_hash" })
+    pass_hash = await browser.cookies.get({ url: server.forums, name: "ipb_pass_hash" });
     member_id = await browser.cookies.get({ url: server.forums, name: "ipb_member_id" });
     if (pass_hash && member_id) {
         if (pass_hash.value === "0" || member_id.value === "0") {
@@ -18,10 +18,14 @@ async function modifyCookie(message, sender, sendResponse) {
         else {
             pass_hash.url = server.ex;
             pass_hash.domain = ".exhentai.org";
+            delete pass_hash.hostOnly;
+            delete pass_hash.session;
             await browser.cookies.set(pass_hash);
 
             member_id.url = server.ex;
             member_id.domain = ".exhentai.org";
+            delete member_id.hostOnly;
+            delete member_id.session;
             await browser.cookies.set(member_id);
 
             return { success: true, msg: "success" }
